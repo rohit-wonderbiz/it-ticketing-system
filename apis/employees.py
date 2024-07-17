ebf1384cdd492331b0d83eec454ddd42986a7fc6
Rohit Wahwal <rohit.wahwal@wonderbiz.in>
2024-07-17 10:28:48 +0530

    Change endpoints for employees and roles

--- apis/employees.py
+++ apis/employees.py
@@ -26,7 +26,7 @@ async def read():
     return "Hello"
 
 # Employees Table GET Method ALL
-@employees.get("/get_all_employees/", response_model=list[EmployeesRead], status_code=status.HTTP_200_OK)
+@employees.get("/employee/", response_model=list[EmployeesRead], status_code=status.HTTP_200_OK)
 async def read_all_employee(db: db_dependency):
     employee_profiles = db.query(Employees).all()
     if not employee_profiles:
@@ -34,7 +34,7 @@ async def read_all_employee(db: db_dependency):
     return employee_profiles
 
 # Employees Table GET Method
-@employees.get("/get_employee_by_id/{emp_Id}", response_model=EmployeesRead, status_code=status.HTTP_200_OK)
+@employees.get("/employee/{emp_Id}", response_model=EmployeesRead, status_code=status.HTTP_200_OK)
 async def read_employee(emp_Id: int, db: db_dependency):
     post = db.query(Employees).filter(Employees.Id == emp_Id).first()
     if post is None:
@@ -42,7 +42,7 @@ async def read_employee(emp_Id: int, db: db_dependency):
     return post
 
 # Employees Table POST Method
-@employees.post("/add_employee/", response_model=EmployeesRead, status_code=status.HTTP_201_CREATED)
+@employees.post("/employee/", response_model=EmployeesRead, status_code=status.HTTP_201_CREATED)
 async def create_employee(emp: EmployeesCreate, db: db_dependency):
     db_post = Employees(**emp.model_dump())
     db.add(db_post)
@@ -51,7 +51,7 @@ async def create_employee(emp: EmployeesCreate, db: db_dependency):
     return db_post
 
 # Employees Table DELETE Method
-@employees.delete("/delete_employee_by_id/{emp_Id}", status_code=status.HTTP_200_OK)
+@employees.delete("/employee/{emp_Id}", status_code=status.HTTP_200_OK)
 async def delete_employee(emp_Id: int, db: db_dependency):
     db_post = db.query(Employees).filter(Employees.Id == emp_Id).first()
     if db_post is None:
@@ -61,7 +61,7 @@ async def delete_employee(emp_Id: int, db: db_dependency):
     return "Employee Deleted!"
 
 # Employees Table EDIT Method
-@employees.put("/edit_employee_by_id/{emp_Id}", response_model=EmployeesRead, status_code=status.HTTP_200_OK)
+@employees.put("/employee/{emp_Id}", response_model=EmployeesRead, status_code=status.HTTP_200_OK)
 async def update_employee(emp_Id: int, updated_post: EmployeesCreate, db: db_dependency):
     db_post = db.query(Employees).filter(Employees.Id == emp_Id).first()
     if db_post is None:
