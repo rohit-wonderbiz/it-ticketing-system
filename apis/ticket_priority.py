a4467aecd2a3c271dbe2600296de18c9fd7174f6
Rohit Wahwal <rohit.wahwal@wonderbiz.in>
2024-07-17 12:03:36 +0530

    Add apis for Ticket Priority

---
+++ apis/ticket_priority.py
@@ -0,0 +1,74 @@
+from fastapi import APIRouter, HTTPException, Depends, status
+from pydantic import BaseModel, Field
+from typing import Annotated, Optional
+from sqlalchemy.orm import Session
+
+from models.model_ticket_priority import TicketPriority #Models
+from models.model_ticket_priority import TicketPriorityBase, TicketPriorityCreate, TicketPriorityRead #Pydantic model
+from database import SessionLocal
+
+ticket_priority = APIRouter()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+# Ticket Priority Table GET Method ALL
+@ticket_priority.get("/", status_code=status.HTTP_200_OK)
+async def read():
+    return "Hello"
+
+# Ticket Priority Table GET Method ALL
+@ticket_priority.get("/ticketPriority/", response_model=list[TicketPriorityRead], status_code=status.HTTP_200_OK)
+async def read_all_ticketPriority(db: db_dependency):
+    employee_ticketsPriority = db.query(TicketPriority).all()
+    if not employee_ticketsPriority:
+        raise HTTPException(status_code=404, detail='No Ticket Priority was found')
+    return employee_ticketsPriority
+
+# Ticket Priority Table GET Method
+@ticket_priority.get("/ticketPriority/{ticketS_Id}", response_model=TicketPriorityRead, status_code=status.HTTP_200_OK)
+async def read_ticketPriority(ticketS_Id: int, db: db_dependency):
+    ticketsPriority = db.query(TicketPriority).filter(TicketPriority.Id == ticketS_Id).first()
+    if ticketsPriority is None:
+        raise HTTPException(status_code=404, detail='Ticket Priority was not found')
+    return ticketsPriority
+
+# Roles Table POST Method
+@ticket_priority.post("/ticketPriority/", response_model=TicketPriorityRead, status_code=status.HTTP_201_CREATED)
+async def create_ticketPriority(emp: TicketPriorityCreate, db: db_dependency):
+    db_post = TicketPriority(**emp.model_dump())
+    db.add(db_post)
+    db.commit()
+    db.refresh(db_post)
+    return db_post
+
+# Ticket Status Table DELETE Method
+@ticket_priority.delete("/ticketPriority/{ticketS_Id}", status_code=status.HTTP_200_OK)
+async def delete_ticketPriority(ticketS_Id: int, db: db_dependency):
+    db_post = db.query(TicketPriority).filter(TicketPriority.Id == ticketS_Id).first()
+    if db_post is None:
+        raise HTTPException(status_code=404, detail='Ticket Priority was not found')
+    db.delete(db_post)
+    db.commit()
+    return "Ticket Priority Deleted!"
+
+# Ticket Priority Table EDIT Method
+@ticket_priority.put("/ticketPriority/{ticketS_Id}", response_model=TicketPriorityRead, status_code=status.HTTP_200_OK)
+async def update_ticketPriority(ticketS_Id: int, updated_post: TicketPriorityCreate, db: db_dependency):
+    db_post = db.query(TicketPriority).filter(TicketPriority.Id == ticketS_Id).first()
+    if db_post is None:
+        raise HTTPException(status_code=404, detail='Ticket Priority was not found')
+
+    for key, value in updated_post.model_dump().items():
+        setattr(db_post, key, value)
+
+    db.commit()
+    db.refresh(db_post)
+    return db_post
--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from apis.employee_systems import employee_systems
 from apis.roles import roles
 from apis.tickets import tickets
 from apis.ticket_status import ticket_status
+from apis.ticket_priority import ticket_priority
 
 # Create all tables in the database
 Base.metadata.create_all(bind=engine)
@@ -20,3 +21,4 @@ app.include_router(employee_systems)
 app.include_router(roles)
 app.include_router(tickets)
 app.include_router(ticket_status)
+app.include_router(ticket_priority)
